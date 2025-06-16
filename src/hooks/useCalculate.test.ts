9a78f7b03c188f55a4cb8597efe6ee073f42d8ea
松井永 <eiciemm@gmail.com>
2025-06-16 21:55:45 +0900

    error test

--- src/hooks/useCalculate.test.ts
+++ src/hooks/useCalculate.test.ts
@@ -5,7 +5,7 @@ const { add, subtract } = useCalculate()
 
 describe('useCalculate', () => {
   test('add 1 + 2 to equal 3', () => {
-    expect(add(1, 2)).toBe(3)
+    expect(add(1, 2)).toBe(30)
   })
 
   test('subtract 10 - 2 to equal 8', () => {
